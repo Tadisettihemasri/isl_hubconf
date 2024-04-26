5e7a536f0f54794a2ecbccdfd52094944fd6beb2
Tadisettihemasri <110807607+Tadisettihemasri@users.noreply.github.com>
2024-04-26 22:21:14 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,13 +1,17 @@
 import torch
 import torchvision.models as models
+from sklearn.datasets import make_blobs
 
-def fun_print():
-	print("Hello World!")
-	return "Hey"
+# def fun_print():
+# 	print("Hello World!")
+# 	return "Hey"
 
-model=models.vgg16(weights='IMAGENET1K_V1')
-torch.save(model.state_dict(), 'model_weights.pth')
+# model=models.vgg16(weights='IMAGENET1K_V1')
+# torch.save(model.state_dict(), 'model_weights.pth')
 
-def get_model():
-	print("model")
-	return model
+# def get_model():
+# 	print("model")
+# 	return model
+def make_blobs:
+	X, y = make_blobs(n_samples=10, centers=3, n_features=2, random_state=0)
+	print(X, y)
